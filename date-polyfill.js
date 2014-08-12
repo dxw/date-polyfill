46195f35e69e615b6d701eda5b6c931b35449fde
Tom Adams <tom@dxw.com>
2014-08-12 16:38:40 -0400

    Initial commit

---
+++ date-polyfill.js
@@ -0,0 +1,9 @@
+jQuery(function () {
+    'use strict';
+
+    if (typeof Modernizr === 'undefined' || typeof Modernizr.inputypes === 'undefined') {
+        console.warn('date-polyfill: Modenizr must be loaded including the inputtypes option')
+    }
+
+    // ...
+})
