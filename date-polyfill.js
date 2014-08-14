26e3b48d2de93fbf91a964789be37dc441351335
Tom Adams <tom@dxw.com>
2014-08-14 10:22:22 -0400

    Basically works

--- date-polyfill.js
+++ date-polyfill.js
@@ -1,9 +1,30 @@
-jQuery(function () {
+jQuery(function ($) {
     'use strict';
 
-    if (typeof Modernizr === 'undefined' || typeof Modernizr.inputypes === 'undefined') {
+    if (typeof Modernizr === 'undefined' || typeof Modernizr.inputtypes === 'undefined') {
         console.warn('date-polyfill: Modenizr must be loaded including the inputtypes option')
+        return
     }
 
-    // ...
+    if (Modernizr.inputtypes.date) {
+        // Our work here is done
+        return
+    }
+
+    $('input[type="date"]').each(function () {
+        var $this = $(this)
+          , humanInput = $(document.createElement('input'))
+
+        humanInput.attr('type', 'text')
+
+        humanInput.datepicker({
+            altField: $this,
+            altFormat: 'yy-mm-dd',
+            dateFormat: 'd MM yy',
+        })
+
+        humanInput.datepicker('setDate', Date.parse($this.val()))
+
+        $this.before(humanInput)
+    })
 })
